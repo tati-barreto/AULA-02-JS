ef8e7b8b39e175577c4d887bd640b068a11eae06
Tatiana Barreto <cbarretotati@gmail.com>
2024-02-14 22:28:18 -0300

    iniciei o codigo

---
+++ script.js
@@ -0,0 +1,10 @@
+let nome = prompt('QUAL SEU NOME?')
+
+alert('OLÁ ' + nome + ', vamos somar 2 números')
+
+let num1 = parseInt(prompt('Digite o primeiro número'))
+let num2 = parseInt(prompt('Digite o segundo número'))
+
+let valor = num1 + num2
+
+alert('O valor da soma é = ' + valor)
